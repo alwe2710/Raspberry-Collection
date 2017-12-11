c3d0e93bf37c98e2c3f26bb4dcc28c17b688f182
alwe2710 <alwe2710@gmail.com>
2017-12-11 18:04:42 +0100

    Analog Potentiometer to Raspberry (1/2)

---
+++ SPIsend.ino
@@ -0,0 +1,14 @@
+int vol = 0;
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  vol = ((analogRead(2) - 22) / 10);
+  if (vol < 0)
+  {
+    vol = 0;
+  }
+  Serial.println(vol);
+
+  delay(10);
+}
